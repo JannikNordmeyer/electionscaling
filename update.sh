d3f998c36b24d7263b090f5bd8214ffc0d1614ed
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2024-10-19 22:43:27 +0200

    Altered Folder Permissions.

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ fi
 cd qual-o-mat-data
 git checkout -b temp
 git pull https://github.com/gockelhahn/qual-o-mat-data.git || { echo An error has occured while updating the Repository.; git checkout master; git branch --delete temp; exit 1; }
-sudo mkdir -p scaled
+sudo mkdir -p -m=rwx  scaled
 
 cd ..
 
